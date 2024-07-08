463ff8948ba547f69a8cbdd6366a7c68d48d377b
SeungBeomLim <21900611@handong.ac.kr>
2024-07-09 05:02:06 +0900

    20240709

--- A110_임승범_20240709.cpp
+++ A110_임승범_20240709.cpp
@@ -1,3 +1,27 @@
 // A110. 거스름돈 - Baekjoon
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> money = {500, 100, 50, 10, 5, 1};
+    int pay;
+    cin >> pay;
+    
+    int res = 1000 - pay;
+    int count = 0, i = 0;
+    while (res > 0) {
+        if (res >= money[i]) {
+            res -= money[i];
+            count++;
+        }
+        else
+            i++;
+    }
+    
+    cout << count;
+    
+    return 0;
+}
