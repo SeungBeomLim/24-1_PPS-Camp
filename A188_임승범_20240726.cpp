cd02bf66a8088016d41748540ed1b7a0ace5683f
SeungBeomLim <21900611@handong.ac.kr>
2024-07-26 03:27:04 +0900

    20240726

---
+++ A188_임승범_20240726.cpp
@@ -0,0 +1,21 @@
+// A188. 사분면 고르기 - Baekjoon
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    
+    if (x > 0 && y > 0)
+       cout << "1" << endl;
+    else if (x < 0 && y > 0)
+        cout << "2" << endl;
+    else if (x < 0 && y < 0)
+        cout << "3" << endl;
+    else if (x > 0 && y < 0)
+        cout << "4" << endl;
+    
+    return 0;
+}
